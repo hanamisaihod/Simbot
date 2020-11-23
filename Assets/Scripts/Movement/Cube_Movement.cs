5dd99f8379d790e5a9a5fa052ff10925642e1117
rompruk21 <58020938+rompruk21@users.noreply.github.com>
2020-11-23 15:02:58 +0700

    Add spawner and receiver prefab

---
+++ Assets/Scripts/Movement/Cube_Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Movement : MonoBehaviour
+{
+    public GameObject cube;
+    void Start()
+    {
+        LeanTween.moveLocalY(gameObject, gameObject.transform.localPosition.y + 0.2f, 1f).setLoopPingPong().setEaseInOutSine();
+        LeanTween.rotateAroundLocal(cube, Vector3.forward, 360, 4f).setLoopClamp();
+        LeanTween.rotateAround(gameObject, Vector3.left, 360, 3f).setLoopClamp();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
