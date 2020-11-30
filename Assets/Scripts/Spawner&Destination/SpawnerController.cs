174160803c238098b062802b15c527732952eab4
rompruk21 <58020938+rompruk21@users.noreply.github.com>
2020-11-30 13:31:32 +0700

    Add spawner and destination effect controller

---
+++ Assets/Scripts/Spawner&Destination/SpawnerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+    public Material floorMat_default;
+    public Material floorMat_taken;
+
+    public GameObject Base;
+    private Renderer BaseRen;
+    public GameObject Core;
+    private Renderer CoreRen;
+    public ParticleSystem AuraFX;
+    public bool ObjectTaken = false;
+
+    void Start()
+    {
+        Core.SetActive(true);
+        AuraFX.Play();
+    }
+
+    
+    void Update()
+    {
+        if (ObjectTaken)
+        {
+            AuraFX.Stop();
+            Core.SetActive(false); // Hide the cube
+            ObjectTaken = false;
+        }
+      
+    }
+}
