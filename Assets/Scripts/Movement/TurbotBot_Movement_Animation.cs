67aaab031be3084dc3f12c7511d7e454e54bd4ad
rompruk21 <58020938+rompruk21@users.noreply.github.com>
2021-01-14 23:22:58 +0700

    Add TurtleBot movement script into prefab
    - Remove old prefabs from oldddd robot
    - Add materials into each robot (with script) prefabs
    - Add TurtleBot movement script into prefab

---
+++ Assets/Scripts/Movement/TurbotBot_Movement_Animation.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Created by Rompruk
+public class TurbotBot_Movement_Animation : MonoBehaviour
+{
+    public GameObject wheel_left;
+    public GameObject wheel_right;
+    public GameObject head;
+    public bool wheelUpdate;
+    public float wheel_L_speed;
+    public float wheel_R_speed;
+    public float delay;
+    public bool headUpdate;
+    public float headDegreeTarget;
+    private float headCurrentDegree;
+    Coroutine headCor;
+    Coroutine wheelCor;
+
+    void Start()
+    {
+        //LeanTween.rotateAround(head, Vector3.up, 720, 1f).setLoopClamp();     // Infinite Head rotating
+        //LeanTween.rotateAround(wheel_left, Vector3.right, 360, 2f).setLoopClamp(); // For test
+        //LeanTween.rotateAround(wheel_right, Vector3.right, 360, 2f).setLoopClamp(); // For test
+    }
+
+    void Update()
+    {
+        if(headUpdate)
+        {
+            if(headCor != null)
+            {
+                StopCoroutine(headCor);
+            }
+            headCor = StartCoroutine(HeadRotating(headDegreeTarget));
+            headUpdate = false;
+        }
+        if(wheelUpdate)
+        {
+            if (wheelCor != null)
+            {
+                StopCoroutine(wheelCor);
+            }
+            wheelCor = StartCoroutine(WheelRotating(wheel_L_speed,wheel_R_speed,delay));
+            wheelUpdate = false;
+        }
+    }
+
+    // Index May be changed later
+    IEnumerator WheelRotating(float L_speed, float R_speed,float rotatingDelay)
+    {
+        LeanTween.rotateAround(wheel_left, Vector3.right, 360 * L_speed * rotatingDelay, rotatingDelay);
+        LeanTween.rotateAround(wheel_right, Vector3.right, 360 * R_speed * rotatingDelay, rotatingDelay);
+        yield return new WaitForSeconds(rotatingDelay);
+    }
+
+    // This too
+    IEnumerator HeadRotating(float degreeTarget)
+    {
+        LeanTween.rotateAround(head, Vector3.up, degreeTarget - headCurrentDegree, 1f);
+        headCurrentDegree = degreeTarget;
+        yield return new WaitForSeconds(1);
+    }
+}
