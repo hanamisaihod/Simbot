e43808e7d4f1bf8ec3ca4dd19fe1f4e9c999c6b8
rompruk21 <58020938+rompruk21@users.noreply.github.com>
2021-01-15 12:41:57 +0700

    Add new sound
    - robot movement
    - new button click sound

---
+++ Assets/Scripts/Movement/StoveBot_Movement_Animation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Created by Rompruk
+public class StoveBot_Movement_Animation : MonoBehaviour
+{
+    public GameObject wheel_left;
+    public GameObject wheel_right;
+    public bool wheelUpdate;
+    public float wheel_L_speed;
+    public float wheel_R_speed;
+    public float delay;
+    Coroutine wheelCor;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (wheelUpdate)
+        {
+            if (wheelCor != null)
+            {
+                StopCoroutine(wheelCor);
+            }
+            wheelCor = StartCoroutine(WheelRotating(wheel_L_speed, wheel_R_speed, delay));
+            wheelUpdate = false;
+        }
+    }
+
+    // Index May be changed later
+    IEnumerator WheelRotating(float L_speed, float R_speed, float rotatingDelay)
+    {
+        LeanTween.rotateAround(wheel_left, Vector3.right, 720 * L_speed * rotatingDelay, rotatingDelay);
+        LeanTween.rotateAround(wheel_right, Vector3.right, 720 * R_speed * rotatingDelay, rotatingDelay);
+        yield return new WaitForSeconds(rotatingDelay);
+    }
+}
