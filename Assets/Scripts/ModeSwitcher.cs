ddbadfe8b345b360af0215c82694b11d2817307b
hanamisaihod <57939244+hanamisaihod@users.noreply.github.com>
2021-03-25 15:09:30 +0700

    Add stageobject in modeswitcher

--- Assets/Scripts/ModeSwitcher.cs
+++ Assets/Scripts/ModeSwitcher.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ModeSwitcher : MonoBehaviour
 {
     public List<GameObject> mapBuildingObjects;
+    public List<GameObject> stageObjects;
     public List<GameObject> blockProgrammingObjects;
     public GameObject pallete;
     public GameObject slideFrame;
@@ -36,7 +37,6 @@ public class ModeSwitcher : MonoBehaviour
 
     private void FindAllStageObjects()
     {
-        List<GameObject> stageObjects = new List<GameObject>();
         stageObjects.AddRange(GameObject.FindGameObjectsWithTag("StageObjects"));
         stageObjects.Add(GameObject.FindGameObjectWithTag("Building"));
         foreach (GameObject obj in stageObjects)
@@ -46,6 +46,13 @@ public class ModeSwitcher : MonoBehaviour
                 mapBuildingObjects.Add(obj);
 			}
 		}
+        for (int i = mapBuildingObjects.Count - 1; i > -1; i--)
+        {
+            if (mapBuildingObjects[i] == null)
+            {
+                mapBuildingObjects.RemoveAt(i);
+            }
+        }
     }
 
     public void SwitchToMap()
