0d6ba86f38f1875f5cfff66248e49a3a6fa707b3
rompruk21 <58020938+rompruk21@users.noreply.github.com>
2021-04-10 14:58:43 +0700

    Add button sound

---
+++ Assets/Scripts/Other/Button_sound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_sound : MonoBehaviour
+{
+    AudioSource Audio;
+    public AudioClip sound;
+
+    void Start()
+    {
+        Audio = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void OnClick()
+    {
+        Audio.Play();
+    }
+
+}
