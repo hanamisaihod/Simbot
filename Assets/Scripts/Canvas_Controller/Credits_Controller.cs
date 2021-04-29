b925466533c5978329cca5784478438122e2052e
rompruk21 <58020938+rompruk21@users.noreply.github.com>
2021-04-29 16:40:34 +0700

    Add end-credits

---
+++ Assets/Scripts/Canvas_Controller/Credits_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits_Controller : MonoBehaviour
+{
+    public GameObject BG;
+    public GameObject script;
+    private Vector3 scriptPosition;
+
+    public void StartCredits()
+    {
+        StartCoroutine(showCredits());
+    }
+
+    IEnumerator showCredits()
+    {
+        script.SetActive(true);
+        scriptPosition = script.transform.localPosition;
+        BG.SetActive(true);
+        LeanTween.moveLocalY(script, script.transform.localPosition.y + 3600f, 32f);
+        yield return new WaitForSeconds(32f);
+        script.transform.localPosition = scriptPosition;
+        script.SetActive(false);
+        BG.SetActive(false);
+    }
+
+}
